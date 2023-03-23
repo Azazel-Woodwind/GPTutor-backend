17ac42d01977956c812e14cfd956ee3c4f6cc07a
Kai <azazelwoodwind11@gmail.com>
2023-03-23 20:32:27 +0500

    Lesson CRUD along with join wait-list API

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 require('dotenv').config();
+const cors = require("cors");
 const express = require('express');
-const superAdminAuth = require('./routes/superAdminAuth');
+const superAdminSignUp = require('./routes/superAdminSignUp');
 const superAdminLogin = require('./routes/superAdminLogin');
-const auth = require('./routes/auth');
-const login = require('./routes/login');
+const adminSignUp = require('./routes/adminSignUp');
+const adminLogin = require('./routes/adminLogin');
 const resetPassword = require('./routes/resetPassword');
+const lessons = require('./routes/lessons');
+const waitList = require('./routes/waitList');
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 //routes
-app.use(express.json());
-app.use('/api/admin-sign-up', superAdminAuth);
-app.use('/api/admin-login', superAdminLogin);
-app.use('/api/register', auth);
-app.use('/api/login', login);
+app.use('/api/super-admin-sign-up', superAdminSignUp);
+app.use('/api/super-admin-login', superAdminLogin);
+app.use('/api/admin-sign-up', adminSignUp);
+app.use('/api/admin-login', adminLogin);
 app.use('/api/reset-password', resetPassword);
+app.use('/api/lessons', lessons);
+app.use('/api/wait-list', waitList);
 
 //Port
 const port = process.env.PORT;
