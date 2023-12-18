3be9587494650a2df048de27e086fb19e4c9638f
Kai <azazelwoodwind11@gmail.com>
2023-12-18 05:46:45 +0100

    chore: update .env.example

--- src/api/http/app.ts
+++ src/api/http/app.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 
 const origin =
     process.env.NODE_ENV === "development"
-        ? process.env.FRONTEND_DEVELOPMENT_ORIGIN
+        ? process.env.FRONTEND_DEVELOPMENT_URL
         : process.env.FRONTEND_PRODUCTION_ORIGIN;
 app.use(cors({ origin }));
 
--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -8,7 +8,7 @@ declare global {
             JWT_SECRET: string;
             SUPABASE_DB_URL: string;
             SUPABASE_SERVICE_ROLE_KEY: string;
-            FRONTEND_DEVELOPMENT_ORIGIN: string;
+            FRONTEND_DEVELOPMENT_URL: string;
             FRONTEND_PRODUCTION_ORIGIN: string;
             MODEL_NAME: string;
             ELEVEN_LABS_API_KEY: string;
